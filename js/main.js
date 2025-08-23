f1216793a50d6cf37c5c47c3eaa76286acadc7ad
Muneeb ur Rehman Alvi <muneeburrehmanalvi@gmail.com>
2025-08-23 22:38:03 +0500

    top up functionality added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
-  const videoModal = document.getElementById('videoModal');
-  const videoFrame = document.getElementById('videoFrame');
+const videoModal = document.getElementById("videoModal");
+const videoFrame = document.getElementById("videoFrame");
 
-  videoModal.addEventListener('show.bs.modal', () => {
-    videoFrame.src += "?autoplay=1";
-  });
+videoModal.addEventListener("show.bs.modal", () => {
+  videoFrame.src += "?autoplay=1";
+});
 
-  videoModal.addEventListener('hidden.bs.modal', () => {
-    videoFrame.src = videoFrame.src.replace("?autoplay=1", "");
-  });
+videoModal.addEventListener("hidden.bs.modal", () => {
+  videoFrame.src = videoFrame.src.replace("?autoplay=1", "");
+});
+
+// top up button
+
+const backToTopBtn = document.getElementById("backToTopBtn");
+
+// Show button when user scrolls down
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+};
+
+// Scroll back to top smoothly
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
